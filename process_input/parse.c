ee22d74ddc9d58f5ccb3ba12a619589ecc9de76f
supriya <supriyaramesh28@gmail.com>
2026-04-12 11:27:42 +0200

    Fix parse emit bash-like stderr for malformed pipe syntax

--- process_input/parse.c
+++ process_input/parse.c
@@ -12,6 +12,51 @@
 
 #include "../minishell.h"
 
+static int	is_pipe_syntax_error(char *input)
+{
+	int	index;
+	int	left;
+	int	right;
+
+	index = 0;
+	while (input[index])
+	{
+		if (input[index] == '|')
+		{
+			left = index - 1;
+			while (left >= 0 && (input[left] == ' ' || input[left] == '\t'))
+				left--;
+			right = index + 1;
+			while (input[right] == ' ' || input[right] == '\t')
+				right++;
+			if (left < 0 || input[right] == '\0'
+				|| input[left] == '|' || input[right] == '|')
+				return (1);
+		}
+		index++;
+	}
+	return (0);
+}
+
+static void	print_pipe_syntax_error(char *input)
+{
+	ft_putstr_fd("bash: line 1: syntax error near unexpected token `|'\n", 2);
+	ft_putstr_fd("bash: line 1: `", 2);
+	ft_putstr_fd(input, 2);
+	ft_putstr_fd("'\n", 2);
+}
+
+static int	handle_cmd_build_failure(t_cmd *cmds, char *input)
+{
+	if (!cmds)
+	{
+		if (is_pipe_syntax_error(input))
+			print_pipe_syntax_error(input);
+		return (2);
+	}
+	return (0);
+}
+
 static int	execute_single_input(char *input, t_env **my_env)
 {
 	t_token	*tokens;
@@ -28,7 +73,7 @@ static int	execute_single_input(char *input, t_env **my_env)
 	}
 	cmds = build_commands(tokens);
 	free_tokens(tokens);
-	if (!cmds)
+	if (handle_cmd_build_failure(cmds, input) != 0)
 		return (2);
 	if (process_heredocs(cmds) < 0)
 	{
