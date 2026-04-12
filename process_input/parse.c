ac1c46d1c04b33a4bea4271da2df9c7417ca1d69
supriya <supriyaramesh28@gmail.com>
2026-04-12 14:01:33 +0200

    Fix parse support unquoted semicolon segments and satisfy norm

--- process_input/parse.c
+++ process_input/parse.c
@@ -6,7 +6,7 @@
 /*   By: spaipur- <spaipur-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 16:33:08 by uvadakku          #+#    #+#             */
-/*   Updated: 2026/04/11 12:32:54 by spaipur-         ###   ########.fr       */
+/*   Updated: 2026/04/12 12:52:30 by spaipur-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,17 +46,6 @@ static void	print_pipe_syntax_error(char *input)
 	ft_putstr_fd("'\n", 2);
 }
 
-static int	handle_cmd_build_failure(t_cmd *cmds, char *input)
-{
-	if (!cmds)
-	{
-		if (is_pipe_syntax_error(input))
-			print_pipe_syntax_error(input);
-		return (2);
-	}
-	return (0);
-}
-
 static int	execute_single_input(char *input, t_env **my_env)
 {
 	t_token	*tokens;
@@ -64,28 +53,53 @@ static int	execute_single_input(char *input, t_env **my_env)
 	int		status;
 
 	tokens = tokenize(input);
-	if (!tokens)
-		return (2);
-	if (expand_token_list(tokens, *my_env) < 0)
-	{
-		free_tokens(tokens);
-		return (2);
-	}
+	if (!tokens || expand_token_list(tokens, *my_env) < 0)
+		return (free_tokens(tokens), 2);
 	cmds = build_commands(tokens);
 	free_tokens(tokens);
-	if (handle_cmd_build_failure(cmds, input) != 0)
-		return (2);
-	if (process_heredocs(cmds) < 0)
+	if (!cmds)
 	{
-		free_cmd_list(cmds);
+		if (is_pipe_syntax_error(input))
+			print_pipe_syntax_error(input);
 		return (2);
 	}
+	if (process_heredocs(cmds) < 0)
+		return (free_cmd_list(cmds), 2);
 	status = execute_commands(cmds, my_env);
 	free_cmd_list(cmds);
 	return (status);
 }
 
+static void	execute_segment(char **seg, char *current,
+			t_env **my_env, int *status)
+{
+	*current = '\0';
+	if (**seg)
+		*status = execute_single_input(*seg, my_env);
+	*seg = current + 1;
+}
+
 int	parse_input(char *input, t_env **my_env)
 {
-	return (execute_single_input(input, my_env));
+	int		quote[2];
+	int		status;
+	char	*seg;
+
+	quote[0] = 0;
+	quote[1] = 0;
+	status = 0;
+	seg = input;
+	while (*input)
+	{
+		if (*input == '\'' && !quote[1])
+			quote[0] = !quote[0];
+		else if (*input == '"' && !quote[0])
+			quote[1] = !quote[1];
+		else if (*input == ';' && !quote[0] && !quote[1])
+			execute_segment(&seg, input, my_env, &status);
+		input++;
+	}
+	if (*seg)
+		status = execute_single_input(seg, my_env);
+	return (status);
 }
